b459eccbe552416d7d56641c4729489533c28628
h662 <h662hong@gmail.com>
2023-11-06 20:36:04 +0900

    Get Weather Data

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 
 const App = () => {
   const [latitude, setLatitude] = useState();
   const [longitude, setLongitude] = useState();
+  const [weatherData, setWeatherData] = useState();
 
-  const getGeolocation = async () => {
+  const getGeolocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLatitude(position.coords.latitude);
+      setLongitude(position.coords.longitude);
+    });
+  };
+
+  const getWeather = async () => {
     try {
-      navigator.geolocation.getCurrentPosition((position) => {
-        setLatitude(position.coords.latitude);
-        setLongitude(position.coords.longitude);
-      });
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
+      );
+
+      setWeatherData(response.data);
+      console.log(response.data);
     } catch (error) {
       console.error(error);
     }
@@ -20,11 +31,27 @@ const App = () => {
   }, []);
 
   useEffect(() => {
-    console.log(latitude);
-    console.log(longitude);
+    if (!latitude || !longitude) return;
+
+    getWeather();
   }, [latitude, longitude]);
 
-  return <div className="bg-red-100">Weather API</div>;
+  return (
+    <div className="bg-pink-100 min-h-screen flex flex-col justify-center items-center">
+      {weatherData ? (
+        <div className="flex flex-col">
+          <img
+            src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
+          />
+          <div>
+            {weatherData.name}, {parseInt(weatherData.main.temp, 10)}℃
+          </div>
+        </div>
+      ) : (
+        <div>Loading...</div>
+      )}
+    </div>
+  );
 };
 
 export default App;
