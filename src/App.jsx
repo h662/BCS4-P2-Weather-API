6d4d3ec421fd28ad3bc312500d5a6e0d56175974
h662 <h662hong@gmail.com>
2023-11-06 20:20:10 +0900

    Get Geolocation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,29 @@
+import { useEffect, useState } from "react";
+
 const App = () => {
+  const [latitude, setLatitude] = useState();
+  const [longitude, setLongitude] = useState();
+
+  const getGeolocation = async () => {
+    try {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setLatitude(position.coords.latitude);
+        setLongitude(position.coords.longitude);
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getGeolocation();
+  }, []);
+
+  useEffect(() => {
+    console.log(latitude);
+    console.log(longitude);
+  }, [latitude, longitude]);
+
   return <div className="bg-red-100">Weather API</div>;
 };
 
